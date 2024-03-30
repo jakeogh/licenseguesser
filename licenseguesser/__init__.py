75c69c5a92994565058834e8698deaf05562376b
jakeogh <github.com@v6y.net>
2024-03-30 11:55:47 -0700

    auto-commit

--- licenseguesser/__init__.py
+++ licenseguesser/__init__.py
@@ -1,2 +1,2 @@
-from .licenseguesser import build_license_dict as license_dict
-from .licenseguesser import build_license_list as license_list
+from .licenseguesser import build_license_dict
+from .licenseguesser import build_license_list
